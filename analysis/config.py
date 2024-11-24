0588aa27a5fdddabf3664e412998de159fb6527c
Zane Gerber <zane.gerber@utah.edu>
2024-11-24 11:05:09 -0700

    remove config.py

--- analysis/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-INPUT_FILE = os.environ.get("ALL_HYBRID")
-TREE_NAME = "taTree"
-OUTPUT_DIR = "/home/zane/software/new_analysis/txHybridDF/results"
-
-DETECTORS = {"MDTAx4": 6, "BRTAx4": 6}
